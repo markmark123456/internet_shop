7349d3fbe7d55c3966372d3ecac7ab7f30c8b296
admin <markmark0489291@gmail.com>
2025-08-08 20:05:38 +0600

    second commit

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/', [MainController::class, 'index']);
+Route::get('/categories', [MainController::class, 'categories']);
+Route::get('/mobiles{product}', [MainController::class, 'product']);
